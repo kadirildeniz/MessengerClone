6dae89f30f5c6b50af164722d58b5d63f4364881
Kadir İldeniz <kadirildeniz5@gmail.com>
2023-08-28 01:31:56 +0300

    auth page form start

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+type Variant = 'LOGIN' | 'REGISTER'
+
+const AuthForm = () => {
+    const [variant, setVariant] = useState<Variant>('LOGIN')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const toggleVariant = useCallback(() => {
+        if(variant === 'LOGIN'){
+            setVariant('REGISTER')
+        } else{
+            setVariant('LOGIN')
+        }
+    }, [])
+
+    const {
+        register,
+        handleSubmit,
+        formState:{
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues:{
+            name:'',
+            email:'',
+            password:'',
+        }
+    })
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true)
+
+        if(variant === 'REGISTER'){
+            //Axios Register
+        }
+
+        if(variant === 'LOGIN'){
+            //NextAuth Sign in
+        }
+    }
+
+    const socialAction = (action:string) =>{
+        setIsLoading(true)
+        //NextAuth Social Sign In
+    }
+    return(
+        <div 
+        className="
+            mt-8
+            sm:mx-auto
+            sm:w-full
+            sm:max-w-md
+        ">
+            <div 
+            className="
+                bg-white
+                px-4
+                py-8
+                shadow
+                sm:rounded-lg
+                sm:px-10
+            ">
+
+            </div>
+
+        </div>
+    );
+}
+
+export default AuthForm;
